816cbb2a782da1b8b7b9776eb25187fdfa6a9023
mrspoon <MrSpoon>
2017-10-29 11:14:08 +0800

    1029.1

--- dispute/Assets/Scripts/UserSign.cs
+++ dispute/Assets/Scripts/UserSign.cs
@@ -1,13 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
+using LitJson;
 public class UserSign : MonoBehaviour {
-
+	public GameObject AccNumInput;
+	public GameObject PWInput;
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+	public void Logon(){
+		StartCoroutine (LogonIn());
+	}
+	IEnumerator LogonIn(){
+		string Info_M = null;
+		Dictionary<string,string> LogonInfo = new Dictionary<string,string>();
+		LogonInfo.Add("Content-Type","application/Json");
+		string date = "{'email':" + AccNumInput.transform.FindChild("Text") + ",'password':" + PWInputText + "}";
+	}
 	// Update is called once per frame
 	void Update () {
 	
