4c06e05c36a2354761f4b7df448e60aee6c7da0f
mrspoon <MrSpoon>
2017-10-31 20:59:35 +0800

    1031.01

--- dispute/Assets/Scripts/UserSign.cs
+++ dispute/Assets/Scripts/UserSign.cs
@@ -8,7 +8,9 @@ public class UserSign : MonoBehaviour {
 	// Use this for initialization
 	private GameObject Center;
 	private GameObject Bg2;
+	private bool Welcome;
 	void Start () {
+		Welcome = false;
 		Center = GameObject.Find ("Center");
 		Bg2 = Center.transform.Find ("BG2").gameObject;
 //		var obj1 = GameObject.Find ("Center");
@@ -26,8 +28,8 @@ public class UserSign : MonoBehaviour {
 		Dictionary<string,string> LogonInfo = new Dictionary<string,string>();
 		LogonInfo.Add("Content-Type", "application/Json");
 		string data = "{'email':" + GameObject.Find("Center/BG1/AccountNumber").GetComponent<InputField>().text + ",'password':" + GameObject.Find("Center/BG1/PassWord").GetComponent<InputField>().text + "}";
-		Debug.Log (GameObject.Find ("Center/BG1/AccountNumber").GetComponent<InputField> ().text);
-		Debug.Log (GameObject.Find ("Center/BG1/PassWord").GetComponent<InputField> ().text);
+//		Debug.Log (GameObject.Find ("Center/BG1/AccountNumber").GetComponent<InputField> ().text);
+//		Debug.Log (GameObject.Find ("Center/BG1/PassWord").GetComponent<InputField> ().text);
 		byte[] bs = System.Text.UTF8Encoding.UTF8.GetBytes (data);
 		WWW www = new WWW ("http://123.56.50.222:8050/userRegister", bs, LogonInfo);
 		yield return www;
@@ -43,7 +45,39 @@ public class UserSign : MonoBehaviour {
 
 			Bg2.transform.Find("Text").GetComponent<Text> ().text = "注册成功";
 		} else {
-			Bg2.transform.Find("Text").GetComponent<Text> ().text = "用户名已被使用，请重新输入";
+			Bg2.transform.Find("Text").GetComponent<Text> ().text = (string)jd["rsttext"];
+		}
+	}
+	public void Sign(){
+		StartCoroutine (SignIn());
+	}
+	IEnumerator SignIn(){
+		string Info_m = null;
+		Dictionary<string,string> SignInfo = new Dictionary<string,string> ();
+		SignInfo.Add ("Content-Type", "application/Json");
+		string data = "{'email':"+GameObject.Find("Center/BG1/AccountNumber").GetComponent<InputField>().text+",'password':"+GameObject.Find("Center/BG1/PassWord").GetComponent<InputField>().text+"}";
+		byte[] bs = System.Text.UTF8Encoding.UTF8.GetBytes (data);
+		WWW www = new WWW ("http://123.56.50.222:8050/userLogin", bs, SignInfo);
+		yield return www;
+		if (www.error != null) {
+			Info_m = www.error;
+			yield return null;
+		}
+		Info_m = www.text;
+		JsonData jd = JsonMapper.ToObject (Info_m);
+		LgBgYes ();
+		if ((string)jd["rstcode"]=="202") {
+			Center.transform.Find ("BG1").gameObject.SetActive (false);
+			LgBgNo ();
+			Welcome = true;
+			Invoke ("ChooseServer",2f);
+		} else {
+			Bg2.transform.Find("Text").GetComponent<Text>().text = (string)jd["rsttext"];
+		}
+	}
+	void OnGUI(){
+		if (Welcome) {
+			GUI.TextArea (new Rect (Center.transform.position.x-50, Center.transform.position.y-10, 100, 20), "欢迎来到纷争！");
 		}
 	}
 	public void LgBgYes(){
@@ -52,8 +86,13 @@ public class UserSign : MonoBehaviour {
 	public void LgBgNo(){
 		Bg2.SetActive (false);
 	}
+	void ChooseServer(){
+		Welcome = false;
+		this.transform.Find ("StartGame").gameObject.SetActive (true);
+		this.transform.Find ("Server").gameObject.SetActive (true);
+	}
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
