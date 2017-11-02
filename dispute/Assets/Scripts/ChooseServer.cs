8e17acb6136765677f97c36ce3b5b2b2bd1fe35a
mrspoon <MrSpoon>
2017-11-02 09:04:54 +0800

    1102.01

--- dispute/Assets/Scripts/ChooseServer.cs
+++ dispute/Assets/Scripts/ChooseServer.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ChooseServer : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+	GameObject[] Btns;
+	GameObject ServerText;
+	void Start(){
+		ServerText = GameObject.Find ("Server/Text");
+		Btns = GameObject.FindGameObjectsWithTag ("button");
+		foreach (GameObject obj in Btns) {
+			GameObject item = GameObject.Find (obj.name);
+			item.GetComponent<Button> ().onClick.AddListener (delegate() {
+				this.Choose (item);
+			});
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void Choose(GameObject obj){
+		ServerText.GetComponent<Text> ().text = obj.transform.Find ("Text").GetComponent<Text> ().text;
+		GameObject.Find ("BG3").SetActive (false);
 	}
 }
